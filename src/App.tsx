4f16139f916e7dc609ccd58337bfff99afc7936f
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2023-02-07 16:13:49 -0500

    Imports entire Vite project
    All files and functions available on localhost.
    
    Connected to cloud DB successfully.
    
    Ready to test in live environment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useContext } from 'react'
+import React from 'react'
+import { useContext, useEffect } from 'react'
 import { MainContext } from './context/main/MainState'
 import { GlobalContext } from './context/global/GlobalState'
+import { getAuth } from 'firebase/auth'
 
 import {
   /* Assets */
+  guestData,
   /* Database */
+  authListener,
   /* Helper Functions */
   /* Components */
+  DisplayPane,
   /* Icons */
 } from './export-hub'
 
@@ -16,92 +21,41 @@ const App = (props: any): JSX.Element => {
     dispatch,
   } = useContext(MainContext)
   const {
-    state: { userObj },
+    
     globalDispatch,
   } = useContext(GlobalContext)
 
-  const callFunc = () => {
-    dispatch({
-      type: 'TOG_MODAL'
-    })
-  }
-
-  const changeUser = () => {
-    globalDispatch({
-      type: 'CHANGE_USER',
-    })
-  }
-
-  const fireTS1 = () => {
-  
- function uuidGenerator50Max(idLength: number = 20, forceString: boolean = false): string | number {
-      let concatString: string = ''
-      for (let i=0; i<3; i++) {
-        concatString = concatString + String(Math.ceil(Math.random() * 10e18)).replaceAll('0', String(Math.floor(Math.random() * 9)))
-      }
-      if (idLength < 18 && !forceString) {
-        return Number(concatString.substring(0, idLength))
-      } else {
-        return concatString.substring(0, idLength)
-      }
-    }
-    
-    console.log(uuidGenerator50Max(12, true))
-  }
-
-  const fireTS2 = () => {
-    function arraySortNumbers(array: (string|number)[], isDescending: boolean = false) {
-      function sortDirection(a: string | number, b: string | number) {
-        a = Number(a)
-        b = Number(b)
-        return isDescending ? b - a : a - b
-      }
-      return array.sort((a, b) => sortDirection(a, b))
-    }
-
-    function arraySortByNumFieldValue(
-      array: any[],
-      keyField: string,
-      isDescending: boolean = false
-    ) {
-      function sortDirection(a: number, b: number) {
-        return isDescending ? b - a : a - b
-      }
-      array.forEach((entry: any) => {
-        
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore - Request for 'ace' object comes from cdn, therefore not available for the typecheck on compilation - no actual functional error
+    if (window.ace) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore - Request for 'ace' object comes from cdn, therefore not available for the typecheck on compilation - no actual functional error
+      let editor = window.ace
+      dispatch({
+        type: 'SET_ACE',
+        payload: { aceObj: editor },
       })
-
-      return array.sort((a, b) => sortDirection(a, b))
     }
+  }, [dispatch])
 
-    let array = [2,5,234,54,'43',23,1,23,4]
-    console.log(arraySortNumbers(array, true))
+  // dispatch({
+  //   type: 'SET_WORKING_OBJECT',
+  //   payload: { workingObject: guestData }
+  // })
+  useEffect(() => {
+    dispatch({
+      type: 'SET_PRIMARY_CATEGORIES',
+      payload: { primaryCategories: [guestData] },
+    })
+  }, [dispatch])
 
-  }
+  let userAuth = getAuth()
+  useEffect(() => {
+    authListener(display, dispatch, globalDispatch, userAuth)
+  }, [dispatch, display, globalDispatch, userAuth])
 
-  return (
-    <div>
-      <button onClick={callFunc}>Toggle Me</button>
-      {display.isModalOpen && (
-        <div
-          style={{
-            width: '200px',
-            height: '200px',
-            backgroundColor: 'lightblue',
-          }}>
-          I'm a Modal
-          <br />
-          <button onClick={changeUser}>Change Me</button>
-          <br />
-          {userObj.name}
-          <br />
-          <button onClick={fireTS1}>Fire TS 1</button>
-          <br />
-          <button onClick={fireTS2}>Fire TS 2</button>
-        </div>
-      )}
-    </div>
-  )
+  return <DisplayPane />
 }
 
 export default App
